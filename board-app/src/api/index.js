61b3054f6d070a295f514a46abd34482f911d0c4
pikiforyou <57664851+pikiforyou@users.noreply.github.com>
2025-07-21 14:28:01 +0900

    Update index.js

--- board-app/src/api/index.js
+++ board-app/src/api/index.js
@@ -2,7 +2,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-	baseURL: process.env.NODE_ENV === 'production' ?' /api/' : 'http://127.0.0.1:8090/',
+	baseURL: /api/,
 })
 
 instance.defaults.headers.post['Content-Type'] =
